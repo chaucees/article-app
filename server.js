11f641048101dacf60477e1ae679f91a2b352669
Chaucee <hi@chaucee.com>
2018-03-15 08:48:11 -0600

    file setup

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+const express = require('express');
+const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const request = require('request');
